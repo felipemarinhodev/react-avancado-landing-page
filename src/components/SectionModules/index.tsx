c566125b4dfd2c284a9ca00ede96117b46904e9f
Felipe Marinho <felipmarinho@gmail.com>
2020-11-27 13:05:13 -0300

    refactor code

--- src/components/SectionModules/index.tsx
+++ src/components/SectionModules/index.tsx
@@ -12,23 +12,17 @@ type Props = {
   sectionModules: SectionModulesProps
 }
 
-const SectionModules = ({ sectionModules }: Props) => {
-  console.log('sectionModule', sectionModules)
-
-  return (
-    <Container>
-      <Heading reverseColor>{sectionModules.title}</Heading>
-      <S.Content>
-        {sectionModules.modules.map(
-          ({ title, subtitle, description }, index) => (
-            <CardModule key={index} title={title} subTitle={subtitle}>
-              <div dangerouslySetInnerHTML={{ __html: description }} />
-            </CardModule>
-          )
-        )}
-      </S.Content>
-    </Container>
-  )
-}
+const SectionModules = ({ sectionModules }: Props) => (
+  <Container>
+    <Heading reverseColor>{sectionModules.title}</Heading>
+    <S.Content>
+      {sectionModules.modules.map(({ title, subtitle, description }, index) => (
+        <CardModule key={index} title={title} subTitle={subtitle}>
+          <div dangerouslySetInnerHTML={{ __html: description }} />
+        </CardModule>
+      ))}
+    </S.Content>
+  </Container>
+)
 
 export default SectionModules
