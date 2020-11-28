a71d1c71424153d5d2d570fbb2963a51b7bdb82a
Felipe Marinho <felipmarinho@gmail.com>
2020-11-28 16:12:06 -0300

    add SectionFaq

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -118,6 +118,16 @@ fragment sectionReviews on LandingPage {
   }
 }
 
+fragment sectionFaq on LandingPage {
+  sectionFaq {
+    title
+    questions {
+      question
+      answer
+    }
+  }
+}
+
 query GET_LANDING_PAGE {
   landingPage {
     ...logo
@@ -130,6 +140,7 @@ query GET_LANDING_PAGE {
 		...pricingBox
 		...sectionAboutUs
 		...sectionReviews
+		...sectionFaq
   }
 }
 `
